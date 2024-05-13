83fa3d22cf86c7b556b8ac6e7bc37acc4e3012ee
Ryan Enslow <enslowryan@gmail.com>
2024-05-12 21:44:44 -0600

    version 1.0 Working

---
+++ Masters_2/STM32CubeIDE/Debug/Application/User/TouchGFX/target/KeySampler.d
@@ -0,0 +1,12 @@
+Application/User/TouchGFX/target/KeySampler.o: \
+ C:/TouchGFXProjects/Masters_2/TouchGFX/target/KeySampler.cpp \
+ C:/TouchGFXProjects/Masters_2/TouchGFX/target/KeySampler.hpp \
+ ../../Middlewares/ST/touchgfx/framework/include/platform/driver/button/ButtonController.hpp \
+ ../../Middlewares/ST/touchgfx/framework/include/touchgfx/hal/Types.hpp \
+ ../../Middlewares/ST/touchgfx/framework/include/touchgfx/hal/Config.hpp \
+ ../../Middlewares/ST/touchgfx/framework/include/touchgfx/hal/Buttons.hpp
+C:/TouchGFXProjects/Masters_2/TouchGFX/target/KeySampler.hpp:
+../../Middlewares/ST/touchgfx/framework/include/platform/driver/button/ButtonController.hpp:
+../../Middlewares/ST/touchgfx/framework/include/touchgfx/hal/Types.hpp:
+../../Middlewares/ST/touchgfx/framework/include/touchgfx/hal/Config.hpp:
+../../Middlewares/ST/touchgfx/framework/include/touchgfx/hal/Buttons.hpp:
---
+++ Masters_2/STM32CubeIDE/Debug/Drivers/BSP/Components/ft6x06.d
@@ -0,0 +1,6 @@
+Drivers/BSP/Components/ft6x06.o: \
+ C:/TouchGFXProjects/Masters_2/Drivers/BSP/Components/ft6x06/ft6x06.c \
+ C:/TouchGFXProjects/Masters_2/Drivers/BSP/Components/ft6x06/ft6x06.h \
+ C:/TouchGFXProjects/Masters_2/Drivers/BSP/Components/ft6x06/../Common/ts.h
+C:/TouchGFXProjects/Masters_2/Drivers/BSP/Components/ft6x06/ft6x06.h:
+C:/TouchGFXProjects/Masters_2/Drivers/BSP/Components/ft6x06/../Common/ts.h:
